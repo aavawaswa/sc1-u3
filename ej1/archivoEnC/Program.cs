2a7fc091c7f19970f93c9a9ea022d1117c66f447
Live session user <pepitoperez@gmail.com>
2022-03-30 09:46:16 -0500

    ejercicio1

---
+++ ej1/archivoEnC/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Queue{
+
+    private int front;
+    private int rear;
+    private double[] arr;
+
+    public Queue(int size){
+
+        front = 0;
+        rear = 0;
+        arr = new double[size];
+    }
+
+    public int size(){
+        return (rear - front);
+    }
+
+    public void enqueue(double item) {
+        arr[rear] = item;
+        rear++;
+    }
+
+    public double dequeue() {
+        double item = arr[front];
+        front++;
+        return item;
+    }
+}
+
+class Program {
+    static void Main() {
+        Queue q = new Queue(10);
+        q.enqueue(6.5);
+        q.enqueue(1.3);
+        q.enqueue(2.4);
+        Console.WriteLine(q.dequeue());
+        Console.WriteLine(q.dequeue());
+        Console.WriteLine(q.dequeue());
+    }
+}
